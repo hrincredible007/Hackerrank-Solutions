77fd26f711139de8bbb830292220be362b744b7e
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2020-09-18 00:18:51 +0530

    Add files via upload

---
+++ Birthday cake Candles.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    c=0
+    k=max(ar)
+    for i in ar:
+        if(k==i):
+            c+=1
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
